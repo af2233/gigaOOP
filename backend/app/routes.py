610c25a8851e1826d7b08e8dd273660ad28a5cfe
af2233 <a.fadeev.101@gmail.com>
2024-05-11 21:56:20 +0300

    renamed from 'sql_main'

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,25 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
-
+from backend.app import crud
+from backend.app.schemas import User, UserCreate
+from backend.app.session import get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post('/users/', response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@app.post('/users/', response_model=User)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
 
     if db_user:
@@ -33,7 +25,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get('/users/{user_id}', response_model=schemas.User)
+@app.get('/users/{user_id}', response_model=User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
 
@@ -43,7 +35,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.delete('/users/{user_id}', response_model=schemas.User)
+@app.delete('/users/{user_id}', response_model=User)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
 
@@ -53,7 +45,7 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     return crud.delete_user(db=db, user_id=user_id)
 
 
-@app.put('/users/{user_id}', response_model=schemas.User)
+@app.put('/users/{user_id}', response_model=User)
 def update_email(user_id: int, email: str, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
 
@@ -68,7 +60,7 @@ def update_email(user_id: int, email: str, db: Session = Depends(get_db)):
     return crud.update_email(db=db, user_id=user_id, email=email)
 
 
-@app.put('/users/{user_id}', response_model=schemas.User)
+@app.put('/users/{user_id}', response_model=User)
 def update_password(user_id: int, password: str, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
 
@@ -78,7 +70,7 @@ def update_password(user_id: int, password: str, db: Session = Depends(get_db)):
     return crud.update_password(db=db, user_id=user_id, password=password)
 
 
-@app.put('/users/{user_id}', response_model=schemas.User)
+@app.put('/users/{user_id}', response_model=User)
 def update_login(user_id: int, login: str, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
 
