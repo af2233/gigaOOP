67cb0685209da109f3863ed2b7372e40ff619def
af2233 <a.fadeev.101@gmail.com>
2024-05-12 11:25:45 +0300

    made it a router

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,33 +1,31 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, APIRouter
 
-from backend.app import crud
+from backend.app.crud import *
 from backend.app.schemas import User, UserCreate
 from backend.app.session import get_db
 
-models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+router = APIRouter(prefix='/users', tags=['User'])
 
 
-@app.post('/users/', response_model=User)
+@router.post('/', response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = get_user_by_email(db, email=user.email)
 
     if db_user:
         raise HTTPException(status_code=400, detail='Email already registered')
 
-    db_user = crud.get_user_by_login(db, login=user.login)
+    db_user = get_user_by_login(db, login=user.login)
 
     if db_user:
         raise HTTPException(status_code=400, detail='Login already registered')
 
-    return crud.create_user(db=db, user=user)
+    return create_user(db=db, user=user)
 
 
-@app.get('/users/{user_id}', response_model=User)
+@router.get('/{user_id}', response_model=User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
 
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
@@ -35,51 +33,53 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.delete('/users/{user_id}', response_model=User)
+@router.delete('/{user_id}', response_model=User)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
 
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
 
-    return crud.delete_user(db=db, user_id=user_id)
+    return delete_user(db=db, user_id=user_id)
 
 
-@app.put('/users/{user_id}', response_model=User)
+@router.put('/{user_id}', response_model=User)
+def update_password(user_id: int, password: str, db: Session = Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    return update_password(db=db, user_id=user_id, password=password)
+
+
+'''
+@router.put('/{user_id}', response_model=User)
 def update_email(user_id: int, email: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
 
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
 
-    db_user = crud.get_user_by_email(db, email=email)
+    db_user = get_user_by_email(db, email=email)
 
     if db_user:
         raise HTTPException(status_code=400, detail='Email already registered')
 
-    return crud.update_email(db=db, user_id=user_id, email=email)
+    return update_email(db=db, user_id=user_id, email=email)
 
 
-@app.put('/users/{user_id}', response_model=User)
-def update_password(user_id: int, password: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-
-    return crud.update_password(db=db, user_id=user_id, password=password)
-
-
-@app.put('/users/{user_id}', response_model=User)
+@router.put('/{user_id}', response_model=User)
 def update_login(user_id: int, login: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
 
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
 
-    db_user = crud.get_user_by_login(db, login=login)
+    db_user = get_user_by_login(db, login=login)
 
     if db_user:
         raise HTTPException(status_code=400, detail='Login already registered')
 
-    return crud.update_login(db=db, user_id=user_id, login=login)
+    return update_login(db=db, user_id=user_id, login=login)
+'''
