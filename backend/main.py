39c41d33f6483b6e07bcafc0f1e728c496605a9b
af2233 <a.fadeev.101@gmail.com>
2024-05-12 11:25:22 +0300

    included users router

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
-app = FastAPI()
+from backend.app.models import Base
+from backend.core.config import settings
+from backend.app.routes import router
+from backend.app.session import engine
 
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+app.include_router(router)
 
 origins = [
-    "http://localhost",
-    "http://localhost:5173",
+    'http://localhost',
+    'http://localhost:5173',
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
-@app.get("/")
+
+@app.get('/')
 def read_root():
     return {'Hello': 'World'}
