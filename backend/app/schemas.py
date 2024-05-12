8adf2597e2c1fec328595d94cbd937dca42d1300
Daniil Makhov <mahovdnl@gmail.com>
2024-05-12 15:34:50 +0300

    fix user schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserBase(BaseModel):
-    # name: str | None = None
-    email: EmailStr
+    name: str | None = None
+    email: str
     password: str = Field(..., min_length=4)
     login: str
     
