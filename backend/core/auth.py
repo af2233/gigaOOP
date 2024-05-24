7dc6c6c560c430e78da8de7bca2bcc4ee39c9239
afade <a.fadeev.101@gmail.com>
2024-05-25 00:04:24 +0300

    new core

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -1,14 +1,14 @@
 from fastapi_users.authentication import AuthenticationBackend, CookieTransport, JWTStrategy
 
-from core.config import Settings
-
+from backend.core.config import settings
 
 
 cookie_transport = CookieTransport(cookie_name='auth', cookie_max_age=3600)
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=Settings.SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=settings.SECRET, lifetime_seconds=3600)
+
 
 auth_backend = AuthenticationBackend(
     name="jwt",
