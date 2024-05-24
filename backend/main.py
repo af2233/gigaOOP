1dc4d38bc26bba050044a62753ab5be59a76c88f
afade <a.fadeev.101@gmail.com>
2024-05-25 00:16:12 +0300

    modified main

--- backend/main.py
+++ backend/main.py
@@ -4,20 +4,18 @@ import uuid
 from fastapi import Depends, FastAPI
 from fastapi_users import FastAPIUsers
 
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+
 from backend.api.routers.course import course_router
 from backend.api.routers.theme import theme_router
-
-
 from backend.api.routers.user import current_active_user, fastapi_users, get_user_manager
 from backend.core.auth import auth_backend
 from backend.core.config import settings
-from backend.db.base import create_db_and_tables
 from backend.db.schemas.user import UserCreate, UserRead, UserUpdate
 from backend.db.models.user import User
+from backend.db.base import create_db_and_tables
 
-
-from fastapi.middleware.cors import CORSMiddleware
-import logging
 
 # Настройка логирования
 logging.basicConfig(level=logging.DEBUG)
