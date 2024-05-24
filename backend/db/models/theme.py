d3ddcb94c885ba7d6885107083ed2e4c2e324659
afade <a.fadeev.101@gmail.com>
2024-05-25 00:05:50 +0300

    added themes

---
+++ backend/db/models/theme.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, ForeignKey, Integer
+
+from backend.db.session import Base
+
+
+class Theme(Base):
+    __tablename__ = "themes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    course_id = Column(Integer, ForeignKey("courses.id"))
