560331d91b7d5895a37ae4fdb7131a9d103e7750
afade <a.fadeev.101@gmail.com>
2024-05-25 18:13:19 +0300

    added pytests

---
+++ backend/tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+import asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from ..main import app
+from ..db.session import Base, get_async_session
+
+TEST_DATABASE_URL = 'sqlite+aiosqlite:///backend/test.db'
+
+engine = create_async_engine(TEST_DATABASE_URL, connect_args={'check_same_thread': False})
+TestingSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+async def override_get_db() -> AsyncSession:
+    async with TestingSessionLocal() as session:
+        yield session
+
+app.dependency_overrides[get_async_session] = override_get_db
+
+
+#  SETUP
+# @pytest.fixture(scope="session")
+# def event_loop(request):
+#     """Create an instance of the default event loop for each test case"""
+#     loop = asyncio.get_event_loop_policy().new_event_loop()
+#     yield loop
+#     loop.close()
+
+
+@pytest.fixture(scope='session')
+async def async_client():
+    """Initializing async client"""
+    async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as client:
+        yield client
+
+
+@pytest.fixture(scope='session', autouse=True)
+async def setup_and_teardown_db():
+    """Setup and teardown test database"""
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
