0495fd6dbb07a890f9be3c3a2710acf6028972cf
afade <a.fadeev.101@gmail.com>
2024-05-25 18:13:02 +0300

    new core

--- backend/core/config.py
+++ backend/core/config.py
@@ -2,7 +2,7 @@ class Settings:
     PROJECT_NAME: str = 'gigaOOP'
     PROJECT_VERSION: str = '0.1.0'
 
-    SQLITE_DB = '/gigaOOP_database.db'
+    SQLITE_DB = '/backend/gigaOOP_database.db'
     DATABASE_URL = f'sqlite+aiosqlite://{SQLITE_DB}'
     SECRET = 'eyJzdWIiOiJkaXNjbyIsIm5hbWUiOiJlbHlzaXVtIiwiaWF0IjoxNTE2MjM5MDIyfQ'
 
