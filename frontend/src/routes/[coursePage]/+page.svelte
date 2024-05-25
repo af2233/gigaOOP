597001cd78ce076b48ba7f7592115d5a349f1d22
Daniil Makhov <mahovdnl@gmail.com>
2024-05-25 20:29:24 +0300

    courses routing boilerplate

--- frontend/src/routes/[coursePage]/+page.svelte
+++ frontend/src/routes/[coursePage]/+page.svelte
@@ -1,19 +1,23 @@
 <script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    console.log(data.par)
 </script>
 
-<style type="text/scss">
-	section{
-		width: 100%;
-		height: 100%;
-		background-color: #f5f5f5;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
 
+<style>
+    section {
+        width: 100%;
+        height: 100%;
+        background-color: #f5f5f5;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+    }
 </style>
 
 <section>
-    <h1>Course page</h1>
+    <h1>{data.course} Course Page</h1>
 </section>
-
