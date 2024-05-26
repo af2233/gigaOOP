b29a25019ae151f1702ad0c26b4a996624be418d
afade <a.fadeev.101@gmail.com>
2024-05-26 19:32:58 +0300

    removed function

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,13 +1,9 @@
 from ..db.session import engine, Base
+from ..db.models.theme import Theme
 from ..db.models.user import User
 from ..db.models.course import Course
-from ..db.models.theme import Theme
-from ..db.models.quiz import Quiz
-from ..db.models.question import Question
-from ..db.models.answer import Answer
-from ..db.models.user_answer import UserAnswer
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+# async def create_db_and_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
