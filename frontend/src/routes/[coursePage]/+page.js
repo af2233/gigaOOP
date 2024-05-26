8a13cb0815b7368321694b6a3840c6431fa32bc5
Senya <yukhnya@gmail.com>
2024-05-27 02:17:58 +0300

    404_page

--- frontend/src/routes/[coursePage]/+page.js
+++ frontend/src/routes/[coursePage]/+page.js
@@ -1,17 +1,27 @@
 import { BASE_URL } from '../../api.js';
+import { error } from '@sveltejs/kit';
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	const courseId = Number(params.coursePage);
-	const res = await fetch(`${BASE_URL}/themes/`);
-	console.log("course page msg")
-    if (res.ok) {
-		const themes = await res.json();
-		console.log(themes);
+    const courseId = Number(params.coursePage);
+    const res = await fetch(`${BASE_URL}/themes/`);
 
-		// Фильтруем темы по course_id
-		const filteredThemes = themes.filter(theme => theme.course_id === courseId);
-		return {
-			themes: filteredThemes
-		};
-	}
+    if (res.ok) {
+        const themes = await res.json();
+        
+        // Фильтруем темы по course_id
+        const filteredThemes = themes.filter(theme => theme.course_id === courseId);
+
+        if (filteredThemes.length === 0) {
+            // Если нет данных для данного courseId, генерируем ошибку 404
+            throw error(404, 'Course not found');
+        }
+
+        return {
+            themes: filteredThemes
+        };
+    } else {
+        // Если fetch запрос не удался, генерируем ошибку
+        throw error(res.status, 'Failed to fetch themes');
+    }
 }
