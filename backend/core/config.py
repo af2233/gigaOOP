a89e05edda1e6a929fbc07dc7e6e462a9e9341ef
af2233 <a.fadeev.101@gmail.com>
2024-06-07 17:20:03 +0300

    added descriptions

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,7 +7,17 @@ load_dotenv()
 
 class Settings:
     PROJECT_NAME: str = "gigaOOP"
+    SUMMARY: str = "IT project - OOP Educational Platform"
+    DESCRIPTION: str = "A web service with basic mechanisms for a working educational platform for studying OOP"
     PROJECT_VERSION: str = "1.0.0"
+    LICENSE_INFO: dict = {"name": "Apache 2.0", "identifier": "MIT"}
+    TAGS_METADATA: list = [
+        {"name": "auth", "description": "The login **logic** is here."},
+        {"name": "users", "description": "Operations with users."},
+        {"name": "courses", "description": ""},
+        {"name": "themes", "description": ""},
+        {"name": "quizzes", "description": ""}
+    ]
 
     DB_NAME = os.environ.get("DB_NAME")
     DB_URL = f"sqlite+aiosqlite:///./{DB_NAME}.db"
--- backend/main.py
+++ backend/main.py
@@ -1,56 +1,60 @@
 import uuid
 import logging
+import uvicorn
 
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_users import FastAPIUsers
 
-from api.routers.user import fastapi_users
+from api.routers import course, quiz, theme, user
 from core.auth import auth_backend
 from core.config import settings
 from db.schemas.user import UserCreate, UserRead, UserUpdate
-from db.models.user import User
-from api.routers import course
-from api.routers import theme
-from api.routers import quiz
 
 
 # Logging setup
 logging.basicConfig(level=logging.DEBUG)
 
 # Main app setup
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+app = FastAPI(title=settings.PROJECT_NAME,
+              summary=settings.SUMMARY,
+              descriptions=settings.DESCRIPTION,
+              version=settings.PROJECT_VERSION,
+              license_info=settings.LICENSE_INFO,
+              openapi_tags=settings.TAGS_METADATA,
+              docs_url=None,
+              redoc_url=None,
+              )
 
 # login/logout routers
 app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
+    user.fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
     tags=["auth"],
 )
 
 # register router
 app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
+    user.fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
 )
 
 app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-    deprecated=True,
-)
-
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
+    user.fastapi_users.get_reset_password_router(),
     prefix="/auth",
     tags=["auth"],
     deprecated=True,
 )
 
 app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    user.fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+    deprecated=True,
+)
+
+app.include_router(
+    user.fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
     tags=["users"],
     deprecated=True,
@@ -86,3 +90,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
