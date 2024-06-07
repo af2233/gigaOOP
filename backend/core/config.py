9180bb3bf859b19aea77e8d8343eb32044ef52a6
af2233 <a.fadeev.101@gmail.com>
2024-06-07 16:36:17 +0300

    secured info with dotenv

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,10 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Settings:
     PROJECT_NAME: str = "gigaOOP"
     PROJECT_VERSION: str = "1.0.0"
 
-    SQLITE_DB = "/./gigaOOP_database.db"
-    DATABASE_URL = f"sqlite+aiosqlite://{SQLITE_DB}"
-    SECRET = "eyJzdWIiOiJkaXNjbyIsIm5hbWUiOiJlbHlzaXVtIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_URL = f"sqlite+aiosqlite:///./{DB_NAME}.db"
+    TEST_DB_NAME = os.environ.get("DB_NAME_TEST")
+    TEST_DB_URL = f"sqlite+aiosqlite:///./{TEST_DB_NAME}.db"
+    SECRET_AUTH = os.environ.get("SECRET_AUTH")
 
 
 settings = Settings()
