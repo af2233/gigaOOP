f5b4bde848d759c3b0019982eb99608dc90fa63d
af2233 <a.fadeev.101@gmail.com>
2025-06-13 14:59:03 +0300

    fix alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -13,6 +13,14 @@ from db.base import Base
 # access to the values within the .ini file in use.
 config = context.config
 
+
+from core.config import settings
+
+url = settings.DB_URL
+
+config.set_main_option("sqlalchemy.url", url)
+
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
